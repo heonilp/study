58e80b13e1e0ccacea5812a24f5459cc5f18f3dc
heonilp <heonil8@nate.com>
2020-12-27 21:14:46 +0900

    jdbc 연습하기
    부스트강의 수업 듣기

---
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -0,0 +1,65 @@
+package kr.or.connect.jdbcexam.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import kr.or.connect.jdbcexam.dto.Role;
+
+public class RoleDao {
+	private static String dburl = "jdbc:mysql://localhost:3306/connectdb?verifyServerCertificate=false&useSSL=false";
+    //문제점 발생해서 ALTER USER 'connectuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'connect123!@#'; 침
+
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
+
+	public Role getRole(Integer roleId) {
+		Role role = null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, roleId);
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String description = rs.getString(1);
+				int id = rs.getInt("role_id");
+				role = new Role(id, description);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return role;
+	}
+}
