ca2ae93a521efe2d81fce78b6f723921e4576cf3
heonilp <heonil8@nate.com>
2020-12-28 16:23:55 +0900

    jdbc java 강의
    부스트코드 강의 연습

---
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam2.java
@@ -0,0 +1,18 @@
+package kr.or.connect.jdbcexam;
+
+import kr.or.connect.jdbcexam.dao.RoleDao;
+import kr.or.connect.jdbcexam.dto.Role;
+
+public class JDBCExam2 {
+	public static void main(String[] args) {
+		int roleId = 501;
+		String description = "CTO";
+		
+		Role role = new Role(roleId, description);
+		
+		RoleDao dao = new RoleDao();
+		int insertCount = dao.addRole(role);
+
+		System.out.println(insertCount);
+	}
+}
--- jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -14,6 +14,30 @@ public class RoleDao {
 
 	private static String dbUser = "connectuser";
 	private static String dbpasswd = "connect123!@#";
+	
+	public int addRole(Role role) {
+		int insertCount = 0;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		String sql = "INSERT INTO role (role_id, description) VALUES ( ?, ? )";
+
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			ps.setInt(1, role.getRoleId());
+			ps.setString(2, role.getDescription());
+
+			insertCount = ps.executeUpdate();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return insertCount;
+	}
 
 	public Role getRole(Integer roleId) {
 		Role role = null;
