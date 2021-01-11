e42f70eba01fd2135d8a7b0951a74a53eeddedbf
heonilp <heonil8@nate.com>
2021-01-11 19:29:05 +0900

    7. 빌더 패턴

---
+++ Java Gof Design Pattern/7. BuilderPattern/src/builder/Builder.java
@@ -0,0 +1,20 @@
+package builder;
+
+public abstract class Builder {
+
+	protected Monster monster;
+	
+	abstract public void setHp();
+	abstract public void setAt();
+	abstract public void setDf();
+	abstract public void setName();
+	
+	public Monster build(){
+		setAt();
+		setDf();
+		setHp();
+		setName();
+		return monster;
+	};
+
+}
---
+++ Java Gof Design Pattern/7. BuilderPattern/src/builder/MonsterDirector.java
@@ -0,0 +1,15 @@
+package builder;
+
+public class MonsterDirector {
+
+	private Builder builder;
+
+	public void setBuilder(Builder builder) {
+		this.builder = builder;
+	}
+	
+	public Monster gen() {
+		return builder.build();
+	}
+
+}
