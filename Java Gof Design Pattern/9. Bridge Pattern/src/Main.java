59f5106aae27af338e18eaed09460a027959895f
heonilp <heonil8@nate.com>
2021-01-13 14:48:44 +0900

    9. 브릿지 패턴 간단한설명추가

--- Java Gof Design Pattern/9. Bridge Pattern/src/Main.java
+++ Java Gof Design Pattern/9. Bridge Pattern/src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
 
+	//브릿지 패턴 : 기능 계층과 구현 계층의 분리
+
+	//브릿지패턴에 대해서이해
+	//어댑터 패턴과 브릿지 패턴을 연결하여 이해
+	// 모스부호로 강의를 찍으셨다함.
 	public static void main(String[] args) {
 		PrintMorseCode code = new PrintMorseCode(new FlashMCF());
 
