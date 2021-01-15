1fb323cd92de66d93b51eda6f386631e05a2462c
heonilp <heonil8@nate.com>
2021-01-15 14:05:14 +0900

    12. Visitor Pattern

--- Java Gof Design Pattern/12. Visitor Pattern/src/visitor/Application.java
+++ Java Gof Design Pattern/12. Visitor Pattern/src/visitor/Application.java
@@ -5,6 +5,12 @@ import visitor.concrete.VisitableA;
 import visitor.concrete.VisitorA;
 import visitor.contract.Visitable;
 
+//방문자 패턴을 이용하여 객체에서 처리를 분리해서 사용할수있다.
+//객체/ 처리/ 분리
+//왜 필요하냐 ? 유지보수
+
+//참고
+//https://thecodinglog.github.io/design/2019/10/29/visitor-pattern.html
 public class Application {
 
 	public static void main(String[] args) {
