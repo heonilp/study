74a3507732b186dff074e547bfd94b9413085015
heonilp <heonil8@nate.com>
2021-01-19 15:08:57 +0900

    18. 메멘토 패턴 설명 추가

--- Java Gof Design Pattern/18. Memento Pattern/memento/Application.java
+++ Java Gof Design Pattern/18. Memento Pattern/memento/Application.java
@@ -7,6 +7,15 @@ import java.util.StringTokenizer;
 import memento.abc.Memento;
 import memento.abc.Originator;
 
+//설명
+//메멘토 : 기념품 ,유품, 추억거리
+//상태의 상태를 저장했다가 이전의 상태로 돌림
+
+//메멘토 패턴을 통해 객체체의 상태를 저장하고 이전의 상태로 복구한다.
+//접근제한자 protected를 이해한다.
+//중요 : protected을 써서 밖에서 값을 수정 못하게 막는다.
+//protected : 상속 받은 내, 패키지 내에서 만 접근 가능
+//키워드 : 복구
 public class Application {
 
 	public static void main(String[] args) {
