f552471925400014bf1a9e3ec3169a0d3f898fcf
heonilp <heonil8@nate.com>
2021-01-19 13:48:20 +0900

    17. 상태패턴 설명추가

--- Java Gof Design Pattern/17. State Pattern/src/state/Application.java
+++ Java Gof Design Pattern/17. State Pattern/src/state/Application.java
@@ -1,5 +1,9 @@
 package state;
 
+//상태를 객체로 나타내기
+//상태 패턴을 통해 상태를 객체로 나타내고 행동을 구현한다.
+//전략패턴 : 알고리즘 바꿈 vs 상태패턴 : 이벤트, 액션을 발생한걸 바꿈(상태를 바꿈)
+
 public class Application {
 	public static void main(String[] args) {
 		Light light = new Light();
