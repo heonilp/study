a3e5a7f17fd32d84fd93d203f6e095d39892c2fc
heonilp <heonil8@nate.com>
2021-02-05 02:07:55 +0900

    leetcode 수정

--- leetcode/Regular Expression Matching.cpp
+++ leetcode/Regular Expression Matching.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-
-        int i = 0;
-        int j = 0;
-        int is = -1;
-        int ip = -1;
-        int n = s.size();
-        int m = p.size();
-
-        while (i < n)
+    bool isMatch(string s, string p)
+    {
+        int sn=s.length();
+        int pn=p.length();
+        
+        vector<vector<bool>> dp(sn+1,vector<bool>(pn+1,0));
+        
+        dp[0][0]=1;
+        
+        for (int j=1; j<=pn; ++j)
         {
-            if( j < m && s[i] == p[j])
+          dp[0][j]=p[j-1] == '*' && dp[0][j-2];  
+        } 
+        
+        for (int i=1; i<=sn; ++i) 
+        {
+            for (int j=1; j<=pn; ++j)
             {
-                i++;
-                j++;
-            }
-            else if(j < m && p[j] == '.')
-            {
-                i++;
-                j++;
-            } 
-            else if(j < m && p[j] == '*')
-            {
-                is = i;
-                ip = j;
-                j++;
-            }
-            else if(ip != -1)
-            {
-                j = ip + 1;
-                i = is + 1;
-                is++;
-            }
-            else
-            {
-                return false;
+                if (isalpha(p[j-1])) dp[i][j]=dp[i-1][j-1]&&s[i-1]==p[j-1];
+                else if (p[j-1]=='.') dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=dp[i][j-2]||dp[i-1][j]&&(s[i-1]==p[j-2]||p[j-2]=='.');
             }
         }
-
-        while(j < m && p[j] == '*')
-        {
-            j++;
-        }
-
-        if(j == m)
-        {
-            return true;
-        }
-
-        return false;
+        
+        return dp[sn][pn];
+        
     }
 };
