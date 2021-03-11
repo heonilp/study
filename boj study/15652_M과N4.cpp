8004d3dd4fe4d4d2f9208fdeeca39ce9277806cc
heonilp <heonil8@nate.com>
2021-03-11 17:38:59 +0900

    boj algorithm #15652

---
+++ boj study/15652_M과N4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int arr[10];
+bool visited[10];
+
+void dfs(int num ,int cnt)
+{
+    if(cnt == m)
+    {
+        for(int i = 0; i < m; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+
+        cout << '\n';
+
+        return;
+    }
+
+    for(int i = num; i <= n; i++)
+    {
+            visited[i] = true;
+            arr[cnt] = i;
+            dfs(i, cnt+1);
+            visited[i] = false;
+    }
+}
+
+int main() 
+{
+    cin >> n >> m;
+    
+    dfs(1,0);
+
+    return 0;
+}
